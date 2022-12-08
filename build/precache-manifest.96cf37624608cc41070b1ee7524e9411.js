eee6a858d634a00f9fcda68f81fdc2fe33b1b48d
suraj patel <vikassuraj283@gmail.com>
2022-12-08 14:01:10 +0530

    :Construction:

---
+++ build/precache-manifest.96cf37624608cc41070b1ee7524e9411.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0c75bd88dd2dbc37f1cc69ae83dd53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "502ee68092f7030d4297",
+    "url": "/static/css/main.b148ea11.chunk.css"
+  },
+  {
+    "revision": "d80d3b65daa1741d915d",
+    "url": "/static/js/2.3b17f11c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3b17f11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502ee68092f7030d4297",
+    "url": "/static/js/main.7bb0662d.chunk.js"
+  },
+  {
+    "revision": "b10d08823a4df677ba67",
+    "url": "/static/js/runtime-main.de931e19.js"
+  },
+  {
+    "revision": "028978f40ca009a385c6fb19486bdc53",
+    "url": "/static/media/book.028978f4.jpg"
+  },
+  {
+    "revision": "67a487675e594f3b274f76d190aa011f",
+    "url": "/static/media/smartphone.67a48767.jpg"
+  },
+  {
+    "revision": "4a0dbe85527b88bfcea004afbf6c9fb8",
+    "url": "/static/media/speaker.4a0dbe85.jpg"
+  }
+]);
